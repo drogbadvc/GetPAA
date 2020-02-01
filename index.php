f953b0636c76fbbc2748600ade6fc7e85bbb0301
Aomine <vincent-capek@laposte.net>
2020-02-01 21:23:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require('vendor/autoload.php');
+
+use Cocur\Slugify\Slugify;
+use csrfhandler\csrf;
+use Plasticbrain\FlashMessages\FlashMessages;
+
+// Csrf for only post/get requests
+$isValid = csrf::post();
+$GetValid = csrf::get();
+$token = csrf::token();
+// Rewrite directory with slugify
+$slugify = new Slugify();
+// Message Flash for errors
+$msg = new FlashMessages();
+
+Storage::session();
+$ppa = new ppa($slugify, $msg);
+
+if ($isValid) {
+    [$keyword, $ppa_record, $depth] = $ppa->inputKeywordsPost();
+    $slug_csv = $slugify->slugify($keyword);
+} else {
+    $ppa->renderError($msg, 'Oops, Token invalide :(');
+}
+
+if (isset($_GET['export']) && $_GET['export'] === 'csv') {
+    if ($GetValid) {
+        $ppa->exportCSV();
+    }
+}
+?>
+
+<?php require_once 'header.php' ?>
+<?php require_once 'contain.php' ?>
